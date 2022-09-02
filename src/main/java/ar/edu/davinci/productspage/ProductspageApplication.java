cf611558d5c05554c9cce7e9d3ab86eea8754a0a
Gabs343 <gabriellopezp343@gmail.com>
2022-09-02 14:06:46 -0300

    uploading proyect

---
+++ src/main/java/ar/edu/davinci/productspage/ProductspageApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.productspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductspageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductspageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/productspage/ProductspageApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.productspage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductspageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
