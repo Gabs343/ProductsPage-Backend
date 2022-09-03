87f480b20cabde824d53830b1ca7a2f3c3c1ec0d
Gabs343 <gabriellopezp343@gmail.com>
2022-09-03 16:48:15 -0300

    Creating the interface of Product Service

---
+++ src/main/java/ar/edu/davinci/productspage/service/ProductService.java
@@ -0,0 +1,25 @@
+package ar.edu.davinci.productspage.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import ar.edu.davinci.productspage.domain.Product;
+import ar.edu.davinci.productspage.exception.BusinessException;
+
+public interface ProductService {
+	
+	Product save(Product product) throws BusinessException;
+	
+	Product update(Product product) throws BusinessException;
+	
+	void delete(Product product);
+	void delete(Long id);
+	
+	List<Product> list();
+	
+	Page<Product> list(Pageable pageable);
+	
+	long count();
+}
