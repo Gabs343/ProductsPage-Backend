0f912dc8393511a9de20955946e4b1647177ae15
Gabs343 <gabriellopezp343@gmail.com>
2022-09-05 13:24:09 -0300

    Creating the response  class

---
+++ src/main/java/ar/edu/davinci/productspage/controller/response/ProductResponse.java
@@ -0,0 +1,25 @@
+package ar.edu.davinci.productspage.controller.response;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+	
+	private Long id;
+	 
+	private String name;
+	
+	private String description;
+	
+	private String type;
+	
+	private BigDecimal basePrice;
+}
