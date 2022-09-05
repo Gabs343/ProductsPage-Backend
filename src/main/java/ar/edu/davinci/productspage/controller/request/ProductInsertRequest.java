fad63fea83110e11aa18395333e0544a10e5b625
Gabs343 <gabriellopezp343@gmail.com>
2022-09-05 13:11:30 -0300

    Creating the request class

---
+++ src/main/java/ar/edu/davinci/productspage/controller/request/ProductInsertRequest.java
@@ -0,0 +1,23 @@
+package ar.edu.davinci.productspage.controller.request;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductInsertRequest {
+	
+	private String name;
+	
+	private String description;
+	
+	private String type;
+	
+	private BigDecimal basePrice;
+}
---
+++ src/main/java/ar/edu/davinci/productspage/controller/request/ProductUpdateRequest.java
@@ -0,0 +1,22 @@
+package ar.edu.davinci.productspage.controller.request;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductUpdateRequest {
+	private String name;
+	
+	private String description;
+	
+	private String type;
+	
+	private BigDecimal basePrice;
+}
