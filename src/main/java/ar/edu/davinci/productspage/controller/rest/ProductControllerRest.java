ce62f1fe10cc721b8668525b8ddd22cce293bbee
Gabs343 <gabriellopezp343@gmail.com>
2022-09-06 21:54:34 -0300

    Creating the controller rest product

--- src/main/java/ar/edu/davinci/productspage/controller/rest/ProductControllerRest.java
+++ src/main/java/ar/edu/davinci/productspage/controller/rest/ProductControllerRest.java
@@ -1,14 +1,30 @@
 package ar.edu.davinci.productspage.controller.rest;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.edu.davinci.productspage.controller.request.ProductInsertRequest;
+import ar.edu.davinci.productspage.controller.request.ProductUpdateRequest;
+import ar.edu.davinci.productspage.controller.response.ProductResponse;
 import ar.edu.davinci.productspage.domain.Product;
+import ar.edu.davinci.productspage.exception.BusinessException;
 import ar.edu.davinci.productspage.service.ProductService;
+import ma.glasnost.orika.MapperFacade;
 
 @RestController
 public class ProductControllerRest extends ShopApp{
@@ -18,8 +34,156 @@ public class ProductControllerRest extends ShopApp{
 	@Autowired
 	private ProductService service;
 	
+	@Autowired
+	private MapperFacade mapper;
+	
+	@GetMapping(path = "/products/all")
 	public List<Product> getList(){
 		LOGGER.info("Products list");
 		return service.list();
+	}
+	
+	@GetMapping(path = "/products")
+	public ResponseEntity<Page<ProductResponse>> getList(Pageable pageable){
+		LOGGER.info("List of paginated products");
+		LOGGER.info("Pageable: " + pageable);
+		
+		Page<ProductResponse> productResponse = null;
+		Page<Product> products = null;
+		
+		try {
+			products = service.list(pageable);
+		}catch(Exception e) {
+			LOGGER.error("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			productResponse = products.map(product -> mapper.map(product, ProductResponse.class));
+		}catch(Exception e) {
+			LOGGER.error("Error: " +  e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<>(productResponse, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/products/{id}")
+	public ResponseEntity<Object> getProduct(@PathVariable Long id){
+		LOGGER.info("Returning the required product");
+		
+		ProductResponse productResponse = null;
+		Product product = null;
+		
+		try {
+			product = service.findById(id);
+		}catch(BusinessException e) {
+			LOGGER.error("Error: " + e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			productResponse = mapper.map(product, ProductResponse.class);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(productResponse, HttpStatus.OK);
+	}
+	
+	@PostMapping(path = "/products")
+	public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductInsertRequest productData){
+		
+		Product product = null;
+		ProductResponse productResponse = null;
+		
+		try {
+			product = mapper.map(productData, Product.class);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			productResponse = mapper.map(product, ProductResponse.class);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Object> updateProduct(@PathVariable("id") Long id, @RequestBody ProductUpdateRequest productData){
+		
+		Product productToModify = null;
+		Product newProduct = null;
+		ProductResponse productResponse = null;
+		
+		try {
+			newProduct = mapper.map(productData, Product.class);
+		}catch(Exception e){
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		try {
+			productToModify = service.findById(id);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+			
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+		
+		if(Objects.nonNull(productToModify)) {
+			productToModify.setName(newProduct.getName());
+			productToModify.setDescription(newProduct.getDescription());
+			productToModify.setType(newProduct.getType());
+			productToModify.setBasePrice(newProduct.getBasePrice());
+			
+			try {
+				productToModify = service.update(productToModify);
+			}catch(BusinessException e) {
+				LOGGER.error(e.getMessage());
+				e.printStackTrace();
+				return new ResponseEntity<>(e.getMessage(), HttpStatus.EXPECTATION_FAILED);
+			}catch(Exception e) {
+				LOGGER.error(e.getMessage());
+				
+				e.printStackTrace();
+				
+				return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+			}
+		}else {
+			LOGGER.error("The Product to modify is null");
+			
+			return new ResponseEntity<>(null, HttpStatus.CREATED);
+		}
+		
+		try {
+			productResponse = mapper.map(productToModify, ProductResponse.class);
+		}catch(Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+	
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") Long id){
+		try {
+			service.delete(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
 	}
 }
