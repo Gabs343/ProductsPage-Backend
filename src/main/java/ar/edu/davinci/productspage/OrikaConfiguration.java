21c8f4bf2d7b1427e8122eaa37eeca76e7da01b0
Gabs343 <gabriellopezp343@gmail.com>
2022-09-09 20:51:10 -0300

    mapping ProductInsertRequest to Product

--- src/main/java/ar/edu/davinci/productspage/OrikaConfiguration.java
+++ src/main/java/ar/edu/davinci/productspage/OrikaConfiguration.java
@@ -11,6 +11,7 @@ import ar.edu.davinci.productspage.controller.request.ProductInsertRequest;
 import ar.edu.davinci.productspage.controller.request.ProductUpdateRequest;
 import ar.edu.davinci.productspage.controller.response.ProductResponse;
 import ar.edu.davinci.productspage.domain.Product;
+import ar.edu.davinci.productspage.domain.ProductType;
 import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
@@ -22,7 +23,7 @@ public class OrikaConfiguration {
 	
 	private final Logger LOGGER = LoggerFactory.getLogger(OrikaConfiguration.class);
 	
-	private ObjectMapper objectMapper;
+	private final ObjectMapper objectMapper;
 	
 	public OrikaConfiguration() {
 		objectMapper = new ObjectMapper();
@@ -33,12 +34,9 @@ public class OrikaConfiguration {
 		MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
 		
 		//PRODUCT
-		mapperFactory.classMap(Product.class, ProductInsertRequest.class).byDefault().register();
-		mapperFactory.classMap(Product.class, ProductUpdateRequest.class).byDefault().register();
-		
 		mapperFactory.classMap(Product.class, ProductResponse.class)
 						.customize(new CustomMapper<Product, ProductResponse>() {
-			public void mapAToB(final Product product, final ProductResponse response, final MappingContext context) {
+			public void mapAtoB(final Product product, final ProductResponse response, final MappingContext context) {
 				LOGGER.info("### Custom mapping for Product --> ProductResponse ###");
 				
 				response.setId(product.getId());
@@ -48,6 +46,23 @@ public class OrikaConfiguration {
 				response.setBasePrice(product.getBasePrice());
 			}
 		}).register();
+		
+		mapperFactory.classMap(ProductInsertRequest.class, Product.class)
+						.customize(new CustomMapper<ProductInsertRequest, Product>(){
+			public void mapAtoB(final ProductInsertRequest insertRequest, final Product product, final MappingContext context) {
+				LOGGER.info("### Custom mapping for ProductIntertRequest --> Product ###");
+				
+				product.setName(insertRequest.getName());
+				product.setDescription(insertRequest.getDescription());
+				
+				ProductType type = ProductType.valueOf(insertRequest.getType());
+				product.setType(type);
+				
+				product.setBasePrice(insertRequest.getBasePrice());
+			}				
+		}).register();
+		
+		mapperFactory.classMap(Product.class, ProductUpdateRequest.class).byDefault().register();
 		
 		return mapperFactory.getMapperFacade();
 	}
