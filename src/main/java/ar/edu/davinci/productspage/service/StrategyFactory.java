d4997f26a9a26430fb3ecc9c17e52fe9e13cb7da
Gabs343 <gabriellopezp343@gmail.com>
2022-09-11 20:42:47 -0300

    Factory that returns the correct strategy

---
+++ src/main/java/ar/edu/davinci/productspage/service/StrategyFactory.java
@@ -0,0 +1,26 @@
+package ar.edu.davinci.productspage.service;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import ar.edu.davinci.productspage.domain.ProductState;
+
+public class StrategyFactory {
+	
+	private Map<ProductState, ProductStateStrategy> strategies = new EnumMap<>(ProductState.class);
+	
+	public StrategyFactory() {
+		initStrategies();
+	}
+	
+	private void initStrategies() {
+		strategies.put(ProductState.NEW, new ProductNew());
+	}
+	
+	public ProductStateStrategy getStrategy(ProductState state) {
+		if(state == null || !strategies.containsKey(state)) {
+			throw new IllegalArgumentException("Invalid " + state);
+		}
+		return strategies.get(state);
+	}
+}
