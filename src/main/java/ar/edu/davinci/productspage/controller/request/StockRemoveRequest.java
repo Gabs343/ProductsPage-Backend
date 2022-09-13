52e32629635de624a7bdb1f0a372a3b47a665ed6
Gabs343 <gabriellopezp343@gmail.com>
2022-09-13 16:21:13 -0300

    new request to the stock

---
+++ src/main/java/ar/edu/davinci/productspage/controller/request/StockRemoveRequest.java
@@ -0,0 +1,14 @@
+package ar.edu.davinci.productspage.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StockRemoveRequest {
+	private Integer quantity;
+}
