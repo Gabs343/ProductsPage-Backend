64c3c1847dfb8b153e27d87dd4065d3a19064b13
Gabs343 <gabriellopezp343@gmail.com>
2022-09-13 15:27:05 -0300

    new table stocks

--- src/main/resources/db/mysql/tables.sql
+++ src/main/resources/db/mysql/tables.sql
@@ -4,9 +4,17 @@
 
 DROP TABLE IF EXISTS products;
 
+DROP TABLE IF EXISTS stocks;
+
 --
 -- CREATE OF TABLES
 --
+
+CREATE TABLE STOCKS(
+	stk_id bigint NOT NULL AUTO_INCREMENT,
+	stk_quantity int DEFAULT NULL,
+	PRIMARY KEY(stk_id)
+);
 
 CREATE TABLE PRODUCTS(
 	pdt_id bigint NOT NULL AUTO_INCREMENT,
@@ -16,6 +24,7 @@ CREATE TABLE PRODUCTS(
 	pdt_final_price decimal(19, 2) DEFAULT NULL,
 	pdt_type_product varchar(255) DEFAULT NULL,
 	pdt_state_product varchar(255) DEFAULT NULL,
-	PRIMARY KEY(pdt_id)
-	
+	pdt_stock_id bigint NOT NULL,
+	CONSTRAINT pdt_stock_fk FOREIGN KEY (pdt_stock_id) REFERENCES stocks(stk_id),
+	PRIMARY KEY(pdt_id)	
 );
